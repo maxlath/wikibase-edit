977acad39d3e7af2ec6c149f9a80c5c79e86c37d
maxlath <k@maxlath.eu>
2017-02-20 11:59:31 +0100

    add reference: validating arguments and adding tests

--- test/reference/add.js
+++ test/reference/add.js
@@ -4,6 +4,10 @@ const addReference = require('../../lib/reference/add')
 const addClaim = require('../../lib/claim/add')
 const { randomString, sandboxEntity } = require('../../lib/tests_utils')
 
+const property = 'P2002'
+const value = randomString()
+const claimPromise = addClaim(CONFIG)(sandboxEntity, property, value)
+
 describe('claim add', () => {
   it('should be a function', done => {
     addReference.should.be.a.Function()
@@ -11,14 +15,55 @@ describe('claim add', () => {
     done()
   })
 
-  // Using an non arrow key to customize the timeout
-  // cf https://github.com/mochajs/mocha/issues/2018
+  it('should rejected if not passed a claim guid', done => {
+    addReference(CONFIG)()
+    .catch(err => {
+      err.message.should.equal('missing guid')
+      done()
+    })
+  })
+
+  it('should rejected if passed an invalid claim guid', done => {
+    addReference(CONFIG)('some-invalid-guid')
+    .catch(err => {
+      err.message.should.equal('invalid guid')
+      done()
+    })
+  })
+
+  // (1)
+  it('should rejected if not passed a URL or entity id', function (done) {
+    this.timeout(20 * 1000)
+    claimPromise
+    .then(res => {
+      const guid = res.claim.id
+      addReference(CONFIG)(guid)
+      .catch(err => {
+        err.message.should.equal('missing reference')
+        done()
+      })
+    })
+  })
+
+  // (1)
+  it('should rejected if passed an invalid reference', function (done) {
+    this.timeout(20 * 1000)
+    claimPromise
+    .then(res => {
+      const guid = res.claim.id
+      addReference(CONFIG)(guid, 'not-a-valid-reference')
+      .catch(err => {
+        err.message.should.equal('invalid reference')
+        done()
+      })
+    })
+  })
+
+  // (1)
   it('should add a reference', function (done) {
     this.timeout(20 * 1000)
-    const property = 'P2002'
-    const value = randomString()
-    const referenceUrl = 'https://example.org/rise-and-box-of-the-holy-sand-box'
-    addClaim(CONFIG)(sandboxEntity, property, value)
+    const referenceUrl = 'https://example.org/rise-and-fall-of-the-holy-sandbox-' + randomString()
+    claimPromise
     .then(res => {
       const guid = res.claim.id
       addReference(CONFIG)(guid, referenceUrl)
@@ -29,3 +74,7 @@ describe('claim add', () => {
     })
   })
 })
+
+// (1)
+// Using an non arrow key to customize the timeout
+// cf https://github.com/mochajs/mocha/issues/2018
