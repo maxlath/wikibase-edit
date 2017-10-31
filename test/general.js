41e7f19b6ba3ebf77b2d44c9b19eadafbe62f1d8
maxlath <k@maxlath.eu>
2017-10-31 15:14:35 +0100

    cleaning arrow functions

--- test/general.js
+++ test/general.js
@@ -6,32 +6,32 @@ const credentialConfig = {
 }
 
 describe('general', () => {
-  it('should be a function', (done) => {
+  it('should be a function', done => {
     wdEdit.should.be.a.Function()
     done()
   })
-  it('should return an object', (done) => {
+  it('should return an object', done => {
     wdEdit(credentialConfig).should.be.an.Object()
     done()
   })
-  it('should have label functions', (done) => {
+  it('should have label functions', done => {
     wdEdit(credentialConfig).label.should.be.a.Object()
     wdEdit(credentialConfig).label.set.should.be.a.Function()
     done()
   })
-  it('should have description functions', (done) => {
+  it('should have description functions', done => {
     wdEdit(credentialConfig).description.should.be.a.Object()
     wdEdit(credentialConfig).description.set.should.be.a.Function()
     done()
   })
-  it('should have alias functions', (done) => {
+  it('should have alias functions', done => {
     wdEdit(credentialConfig).alias.should.be.a.Object()
     wdEdit(credentialConfig).alias.add.should.be.a.Function()
     wdEdit(credentialConfig).alias.remove.should.be.a.Function()
     wdEdit(credentialConfig).alias.set.should.be.a.Function()
     done()
   })
-  it('should have claim functions', (done) => {
+  it('should have claim functions', done => {
     wdEdit(credentialConfig).claim.should.be.a.Object()
     wdEdit(credentialConfig).claim.exists.should.be.a.Function()
     wdEdit(credentialConfig).claim.add.should.be.a.Function()
@@ -39,18 +39,18 @@ describe('general', () => {
     wdEdit(credentialConfig).claim.remove.should.be.a.Function()
     done()
   })
-  it('should have qualifier functions', (done) => {
+  it('should have qualifier functions', done => {
     wdEdit(credentialConfig).qualifier.should.be.a.Object()
     wdEdit(credentialConfig).qualifier.add.should.be.a.Function()
     done()
   })
-  it('should have reference functions', (done) => {
+  it('should have reference functions', done => {
     wdEdit(credentialConfig).reference.should.be.a.Object()
     wdEdit(credentialConfig).reference.add.should.be.a.Function()
     wdEdit(credentialConfig).reference.remove.should.be.a.Function()
     done()
   })
-  it('should have entity functions', (done) => {
+  it('should have entity functions', done => {
     wdEdit(credentialConfig).entity.should.be.a.Object()
     wdEdit(credentialConfig).entity.create.should.be.a.Function()
     wdEdit(credentialConfig).entity.edit.should.be.a.Function()
@@ -59,25 +59,25 @@ describe('general', () => {
 })
 
 describe('with credentials', () => {
-  it('should throw if not passed a username', (done) => {
+  it('should throw if not passed a username', done => {
     (() => wdEdit({ password: 'bla' })).should.throw()
     done()
   })
-  it('should throw if not passed a password', (done) => {
+  it('should throw if not passed a password', done => {
     (() => wdEdit({ username: 'bla' })).should.throw()
     done()
   })
 })
 
 describe('with oauth', () => {
-  it('should not throw if passed an oauth object', (done) => {
+  it('should not throw if passed an oauth object', done => {
     (() => wdEdit({ oauth: {} })).should.not.throw()
     done()
   })
 })
 
 describe('per-function initialization', () => {
-  it('should give access to a function without calling the whole initializer', (done) => {
+  it('should give access to a function without calling the whole initializer', done => {
     wdEdit(credentialConfig, 'claim/add').should.be.a.Function()
     done()
   })
--- test/log.js
+++ test/log.js
@@ -3,33 +3,33 @@ const { log, Log, LogError } = require('../lib/log')({ verbose: true })
 
 describe('loggers', () => {
   describe('log', () => {
-    it('should be a function', (done) => {
+    it('should be a function', done => {
       log.should.be.a.Function()
       done()
     })
-    it('should return what it logs', (done) => {
+    it('should return what it logs', done => {
       log('bla', 123).should.equal(123)
       done()
     })
   })
   describe('Log', () => {
-    it('should be a function', (done) => {
+    it('should be a function', done => {
       Log.should.be.a.Function()
       Log('bla').should.be.a.Function()
       done()
     })
-    it('should return what it logs', (done) => {
+    it('should return what it logs', done => {
       Log('bla')(123).should.equal(123)
       done()
     })
   })
   describe('LogError', () => {
-    it('should be a function', (done) => {
+    it('should be a function', done => {
       LogError.should.be.a.Function()
       LogError('bla').should.be.a.Function()
       done()
     })
-    it('should return what it logs', (done) => {
+    it('should return what it logs', done => {
       const err = new Error('doh')
       const fn = () => LogError('bla')(err)
       fn.should.throw()
