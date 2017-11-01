ffac13be60fc07434c6cfcc2134b05259d27b4eb
maxlath <k@maxlath.eu>
2017-11-01 12:08:27 +0100

    logs: introduce levels of verbosity to get only the error messages when running tests

---
+++ config/default.js
@@ -0,0 +1,11 @@
+module.exports = {
+  username: 'your-wikidata-username',
+  password: 'your-wikidata-password',
+  verbose: 2
+  // oauth: {
+  //   consumer_key: 'your-consumer-token',
+  //   consumer_secret: 'your-secret-token',
+  //   token: 'a-user-token',
+  //   token_secret: 'a-secret-token'
+  // }
+}
