664eff447aaafdd48e53d1a719af7100f4ec4d72
maxlath <k@maxlath.eu>
2018-03-03 17:56:32 +0100

    get_time_object: refactoring and cleaning

--- test/claim/time.js
+++ test/claim/time.js
@@ -1,17 +1,16 @@
-const should = require('should')
-const time = require('../../lib/claim/get_time_object')
+require('should')
+const getTimeObject = require('../../lib/claim/get_time_object')
 
 describe('claim time', function () {
   this.timeout(20 * 1000)
 
   it('should be a function', done => {
-    time.should.be.a.Function()
+    getTimeObject.should.be.a.Function()
     done()
   })
 
   it('should parse year without precision', function (done) {
-    const t = time('2018')
-    t.should.deepEqual({
+    getTimeObject('2018').should.deepEqual({
       'time': '+2018-00-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
@@ -23,8 +22,7 @@ describe('claim time', function () {
   })
 
   it('should parse month without precision', function (done) {
-    const t = time('2018-03')
-    t.should.deepEqual({
+    getTimeObject('2018-03').should.deepEqual({
       'time': '+2018-03-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
@@ -36,8 +34,7 @@ describe('claim time', function () {
   })
 
   it('should parse day without precision', function (done) {
-    const t = time('2018-03-03')
-    t.should.deepEqual({
+    getTimeObject('2018-03-03').should.deepEqual({
       'time': '+2018-03-03T00:00:00Z',
       'timezone': 0,
       'before': 0,
@@ -48,9 +45,8 @@ describe('claim time', function () {
     done()
   })
 
-  it('should parse year wit precision ', function (done) {
-    const t = time('2018', 9)
-    t.should.deepEqual({
+  it('should parse year with precision', function (done) {
+    getTimeObject('2018', 9).should.deepEqual({
       'time': '+2018-00-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
@@ -62,8 +58,7 @@ describe('claim time', function () {
   })
 
   it('should parse month with precision', function (done) {
-    const t = time('2018-03', 10)
-    t.should.deepEqual({
+    getTimeObject('2018-03', 10).should.deepEqual({
       'time': '+2018-03-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
@@ -75,8 +70,7 @@ describe('claim time', function () {
   })
 
   it('should parse day with precision', function (done) {
-    const t = time('2018-03-03', 11)
-    t.should.deepEqual({
+    getTimeObject('2018-03-03', 11).should.deepEqual({
       'time': '+2018-03-03T00:00:00Z',
       'timezone': 0,
       'before': 0,
@@ -88,8 +82,7 @@ describe('claim time', function () {
   })
 
   it('should parse decade with precision', function (done) {
-    const t = time('2010', 8)
-    t.should.deepEqual({
+    getTimeObject('2010', 8).should.deepEqual({
       'time': '+2010-00-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
@@ -101,8 +94,7 @@ describe('claim time', function () {
   })
 
   it('should parse century with precision', function (done) {
-    const t = time('2100', 7)
-    t.should.deepEqual({
+    getTimeObject('2100', 7).should.deepEqual({
       'time': '+2100-00-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
@@ -114,8 +106,7 @@ describe('claim time', function () {
   })
 
   it('should parse millenium with precision', function (done) {
-    const t = time('2000', 6)
-    t.should.deepEqual({
+    getTimeObject('2000', 6).should.deepEqual({
       'time': '+2000-00-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
@@ -127,8 +118,7 @@ describe('claim time', function () {
   })
 
   it('should parse ten thousand years with precision', function (done) {
-    const t = time('-10000', 5)
-    t.should.deepEqual({
+    getTimeObject('-10000', 5).should.deepEqual({
       'time': '-10000-00-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
@@ -140,9 +130,8 @@ describe('claim time', function () {
   })
 
   it('should parse hundred thousand years with precision', function (done) {
-    const t = time('-2500000', 4)
-    t.should.deepEqual({
-      'time': '-2500000-01-01T00:00:00Z',
+    getTimeObject('-2500000', 4).should.deepEqual({
+      'time': '-2500000-00-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
       'after': 0,
@@ -153,9 +142,8 @@ describe('claim time', function () {
   })
 
   it('should parse million years with precision', function (done) {
-    const t = time('-13798000000', 3)
-    t.should.deepEqual({
-      'time': '-13798000000-01-01T00:00:00Z',
+    getTimeObject('-13798000000', 3).should.deepEqual({
+      'time': '-13798000000-00-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
       'after': 0,
@@ -166,8 +154,7 @@ describe('claim time', function () {
   })
 
   it('should parse billion years with precision', function (done) {
-    const t = time('-5000000000', 0)
-    t.should.deepEqual({
+    getTimeObject('-5000000000', 0).should.deepEqual({
       'time': '-5000000000-00-00T00:00:00Z',
       'timezone': 0,
       'before': 0,
