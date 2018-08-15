61d965ead3afd01ccfde37a846c3b54b8b02ab7a
maxlath <k@maxlath.eu>
2018-08-15 13:08:45 +0200

    entity: create: added a test based on test.wikidata.org

--- test/entity/create.js
+++ test/entity/create.js
@@ -1,8 +1,14 @@
 require('should')
-const CONFIG = require('config')
+const CONFIG = Object.assign({}, require('config'), {
+  wikibaseInstance: 'https://test.wikidata.org/w/api.php'
+})
 const createEntity = require('../../lib/entity/create')(CONFIG)
+const { randomString } = require('../../lib/tests_utils')
+const wdk = require('wikidata-sdk')
 
-describe('entity edit', () => {
+describe('entity edit', function () {
+  this.timeout(20 * 1000)
+
   it('should be a function', done => {
     createEntity.should.be.a.Function()
     done()
@@ -12,6 +18,26 @@ describe('entity edit', () => {
     createEntity({ claims: { P31: 'bla' } })
     .catch(err => {
       err.message.should.equal('invalid entity value')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should create an entity', done => {
+    const label = `wikidata-edit entity.create test (${randomString()})`
+    const description = randomString()
+    createEntity({
+      labels: { en: label },
+      aliases: { fr: randomString(), en: [ randomString() ] },
+      descriptions: { fr: description },
+      claims: { P17: 'Q166376' }
+    })
+    .then(res => {
+      res.success.should.equal(1)
+      wdk.isItemId(res.entity.id).should.be.true()
+      res.entity.labels.en.value.should.equal(label)
+      res.entity.descriptions.fr.value.should.equal(description)
+      wdk.simplify.claim(res.entity.claims.P17[0]).should.equal('Q166376')
       done()
     })
     .catch(done)
