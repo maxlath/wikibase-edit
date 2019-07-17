5205e0e1a1e7fbe9836114d5e7b14b34dee280ec
maxlath <k@maxlath.eu>
2019-07-17 20:44:32 +0200

    errors: make use of error_.new function

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const parseInstance = require('./lib/parse_instance')
 const requestWrapper = require('./lib/request_wrapper')
+const error_ = require('./lib/error')
 
 module.exports = function (config, functionPath) {
-  if (typeof config !== 'object') throw new Error('missing config object')
+  if (typeof config !== 'object') error_.new('missing config object', { config })
 
   const instance = parseInstance(config)
   config.wbk = require('wikibase-sdk')({ instance })
 
   // Oauth config will be validated by wikidata-token
   if (!config.oauth) {
-    if (!config.username) throw new Error('missing config parameter: username')
-    if (!config.password) throw new Error('missing config parameter: password')
+    if (!config.username) error_.new('missing config parameter: username', { config })
+    if (!config.password) error_.new('missing config parameter: password', { config })
   }
 
   return {
