9cfda5e71f3afef7937a8508ce624ee16e2afe5e
maxlath <k@maxlath.eu>
2019-07-17 20:07:45 +0200

    tests: updated summary tests

---
+++ test/integration/summary.js
@@ -0,0 +1,48 @@
+require('should')
+const config = require('config')
+const wbEdit = require('../..')(config)
+const { randomString } = require('../utils')
+const { getSandboxItem, getSandboxProperty } = require('./utils')
+const breq = require('bluereq')
+
+describe('summary', function () {
+  this.timeout(20 * 1000)
+
+  it('should add a default summary', done => {
+    doSomeEdit(config)
+    .then(summary => {
+      summary.should.endWith('#wikidatajs/edit')
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should accept a custom summary in config', done => {
+    // Not cloning the config object as it is modified internally
+    const summary = 'some custom summary'
+    const customConfig = Object.assign({}, config, { summary })
+    doSomeEdit(customConfig)
+    .then(summary => {
+      summary.should.endWith(summary)
+      done()
+    })
+    .catch(done)
+  })
+})
+
+const doSomeEdit = config => {
+  return Promise.all([
+    getSandboxItem(),
+    getSandboxProperty('string')
+  ])
+  .then(([ id, property ]) => {
+    const value = randomString(4)
+    return wbEdit.claim.add(config, { id, property, value })
+    .then(res => {
+      const wbk = require('wikibase-sdk')(config)
+      const url = wbk.getRevisions(`Item:${id}`, { limit: 1 })
+      return breq.get(url).get('body')
+    })
+    .then(res => Object.values(res.query.pages)[0].revisions[0].comment)
+  })
+}
