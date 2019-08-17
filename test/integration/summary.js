95fe1d294e651d401c71d44682ac7476df5319ed
maxlath <k@maxlath.eu>
2019-08-17 14:32:03 +0200

    summary: allow to pass a general summary or a per-request summary

--- test/integration/summary.js
+++ test/integration/summary.js
@@ -1,51 +1,57 @@
 require('should')
 const config = require('config')
-const { __ } = config
-const wbEdit = __.require('.')(config)
+const { instance, credentials, __ } = config
+const wbk = require('wikibase-sdk')({ instance })
+const WBEdit = __.require('.')
 const { randomString } = require('../unit/utils')
-const { getSandboxItemId, getSandboxPropertyId } = __.require('test/integration/utils/sandbox_entities')
 const breq = require('bluereq')
+const params = () => ({ labels: { en: randomString() } })
+const toolSignature = '#wikidatajs/edit'
 
 describe('summary', function () {
   this.timeout(20 * 1000)
   before('wait for instance', __.require('test/integration/utils/wait_for_instance'))
 
   it('should add a default summary', done => {
-    doSomeEdit(config)
-    .then(summary => {
-      summary.should.endWith('#wikidatajs/edit')
+    const wbEdit = WBEdit({ instance, credentials })
+    wbEdit.entity.create(params())
+    .then(getEditSummary)
+    .then(editSummary => {
+      editSummary.should.endWith(toolSignature)
       done()
     })
     .catch(done)
   })
 
   it('should accept a custom summary in config', done => {
-    // Not cloning the config object as it is modified internally
     const summary = 'some custom summary'
-    const customConfig = Object.assign({}, config, { summary })
-    doSomeEdit(customConfig)
-    .then(summary => {
-      summary.should.endWith(summary)
+    const customConfig = Object.assign({ instance, credentials, summary })
+    const wbEdit = WBEdit(customConfig)
+    wbEdit.entity.create(params())
+    .then(getEditSummary)
+    .then(editSummary => {
+      editSummary.should.endWith(`${summary} ${toolSignature}`)
+      done()
+    })
+    .catch(done)
+  })
+
+  it('should accept a custom summary in the request config', done => {
+    const summary = 'another custom summary'
+    const wbEdit = WBEdit({ instance, credentials })
+    wbEdit.entity.create(params(), { summary })
+    .then(getEditSummary)
+    .then(editSummary => {
+      editSummary.should.endWith(`${summary} ${toolSignature}`)
       done()
     })
     .catch(done)
   })
 })
 
-const doSomeEdit = config => {
-  return Promise.all([
-    getSandboxItemId(),
-    getSandboxPropertyId('string')
-  ])
-  .then(([ id, property ]) => {
-    const value = randomString()
-    return wbEdit.claim.create({ id, property, value })
-    .then(res => {
-      const { instance } = config
-      const wbk = require('wikibase-sdk')({ instance })
-      const url = wbk.getRevisions(`Item:${id}`, { limit: 1 })
-      return breq.get(url).get('body')
-    })
-    .then(res => Object.values(res.query.pages)[0].revisions[0].comment)
-  })
+const getEditSummary = res => {
+  const { id } = res.entity
+  const url = wbk.getRevisions(`Item:${id}`, { limit: 1 })
+  return breq.get(url).get('body')
+  .then(res => Object.values(res.query.pages)[0].revisions[0].comment)
 }
