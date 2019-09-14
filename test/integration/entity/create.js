da3e5c5b0fbdffdc7c1a2f959776dd3f7f75f2bd
maxlath <k@maxlath.eu>
2019-09-14 10:49:21 +0200

    tests: entity: create: add test for property creation

--- test/integration/entity/create.js
+++ test/integration/entity/create.js
@@ -9,6 +9,22 @@ describe('entity create', function () {
   this.timeout(20 * 1000)
   before('wait for instance', __.require('test/integration/utils/wait_for_instance'))
 
+  it('should create a property', done => {
+    wbEdit.entity.create({
+      type: 'property',
+      datatype: 'external-id',
+      labels: {
+        en: randomString()
+      }
+    })
+    .then(res => {
+      res.success.should.equal(1)
+      res.entity.type.should.equal('property')
+      done()
+    })
+    .catch(done)
+  })
+
   it('should create an item', done => {
     Promise.all([
       getSandboxPropertyId('string'),
