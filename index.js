54d997a94520b2d7142354f2f90bb4730d1c4648
maxlath <k@maxlath.eu>
2019-10-01 22:46:13 +0200

    index: add alias qualifier.add = qualifier.set
    to support legacy naming

--- index.js
+++ index.js
@@ -43,7 +43,9 @@ module.exports = (initConfig = {}) => {
     }
   }
 
+  // Legacy aliases
   API.claim.add = API.claim.create
+  API.qualifier.add = API.qualifier.create
 
   // Bundles: async functions that make use of the primitives to offer more sophisticated behaviors
   API.claim.update = bundleWrapper('claim/update', initConfig, API)
