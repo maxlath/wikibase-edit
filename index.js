06d3285af802d810fa3245f84de27817545fff36
maxlath <k@maxlath.eu>
2019-10-01 21:36:08 +0200

    index: add alias claim.add = claim.create
    to support legacy naming

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ module.exports = (initConfig = {}) => {
     }
   }
 
+  API.claim.add = API.claim.create
+
   // Bundles: async functions that make use of the primitives to offer more sophisticated behaviors
   API.claim.update = bundleWrapper('claim/update', initConfig, API)
   API.qualifier.update = bundleWrapper('qualifier/update', initConfig, API)
