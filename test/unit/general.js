b303a55b9a9eafe2e225b7912eaf5dcdc9ab7e6c
maxlath <k@maxlath.eu>
2019-12-13 17:43:16 +0100

    test: unit: general: cleanup tests

--- test/unit/general.js
+++ test/unit/general.js
@@ -1,53 +1,47 @@
 require('should')
 const { __ } = require('config')
 const wdEdit = __.require('.')
-const instance = 'https://www.wikidata.org'
-const credentialConfig = {
-  instance,
-  username: 'bla',
-  password: 'bla'
-}
 
 describe('general', () => {
   it('should return an object', done => {
-    wdEdit(credentialConfig).should.be.an.Object()
+    wdEdit({}).should.be.an.Object()
     done()
   })
   it('should have label functions', done => {
-    wdEdit(credentialConfig).label.set.should.be.a.Function()
+    wdEdit({}).label.set.should.be.a.Function()
     done()
   })
   it('should have description functions', done => {
-    wdEdit(credentialConfig).description.set.should.be.a.Function()
+    wdEdit({}).description.set.should.be.a.Function()
     done()
   })
   it('should have alias functions', done => {
-    wdEdit(credentialConfig).alias.set.should.be.a.Function()
-    wdEdit(credentialConfig).alias.add.should.be.a.Function()
-    wdEdit(credentialConfig).alias.remove.should.be.a.Function()
+    wdEdit({}).alias.set.should.be.a.Function()
+    wdEdit({}).alias.add.should.be.a.Function()
+    wdEdit({}).alias.remove.should.be.a.Function()
     done()
   })
   it('should have claim functions', done => {
-    wdEdit(credentialConfig).claim.create.should.be.a.Function()
-    wdEdit(credentialConfig).claim.update.should.be.a.Function()
-    wdEdit(credentialConfig).claim.remove.should.be.a.Function()
+    wdEdit({}).claim.create.should.be.a.Function()
+    wdEdit({}).claim.update.should.be.a.Function()
+    wdEdit({}).claim.remove.should.be.a.Function()
     done()
   })
   it('should have qualifier functions', done => {
-    wdEdit(credentialConfig).qualifier.set.should.be.a.Function()
-    // wdEdit(credentialConfig).qualifier.update.should.be.a.Function()
-    wdEdit(credentialConfig).qualifier.remove.should.be.a.Function()
+    wdEdit({}).qualifier.set.should.be.a.Function()
+    wdEdit({}).qualifier.update.should.be.a.Function()
+    wdEdit({}).qualifier.remove.should.be.a.Function()
     done()
   })
   it('should have reference functions', done => {
-    wdEdit(credentialConfig).reference.set.should.be.a.Function()
-    // wdEdit(credentialConfig).qualifier.update.should.be.a.Function()
-    wdEdit(credentialConfig).reference.remove.should.be.a.Function()
+    wdEdit({}).reference.set.should.be.a.Function()
+    wdEdit({}).qualifier.update.should.be.a.Function()
+    wdEdit({}).reference.remove.should.be.a.Function()
     done()
   })
   it('should have entity functions', done => {
-    wdEdit(credentialConfig).entity.create.should.be.a.Function()
-    wdEdit(credentialConfig).entity.edit.should.be.a.Function()
+    wdEdit({}).entity.create.should.be.a.Function()
+    wdEdit({}).entity.edit.should.be.a.Function()
     done()
   })
 })
