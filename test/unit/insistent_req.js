5b194a039f788b4823e1f68e0f58ab55addbcbc4
maxlath <k@maxlath.eu>
2020-04-24 11:45:16 +0200

    Revert "tests: insistent_req: rewrite test without extra dependency"
    This reverts commit b3e70eab52aecf1f488d5a6c3b3575cc5cf1de42.
    
    Following comment https://github.com/maxlath/wikibase-edit/issues/53#issuecomment-618909756

--- test/unit/insistent_req.js
+++ test/unit/insistent_req.js
@@ -1,17 +1,23 @@
 require('should')
 const { __ } = require('config')
 const insistentReq = __.require('lib/request/insistent_req')
+const nock = require('nock')
 const { shouldNotGetHere, rethrowShouldNotGetHereErrors } = require('../integration/utils/utils')
-const someUrlThatReturnsHtml = 'https://www.wikidata.org/w/api.php?action=help&modules=rollback'
 
 describe('insistent_req', () => {
-  it("should throw a wrong format error when the response isn't JSON", async () => {
+  beforeEach(() => {
+    nock('https://example.org')
+    .get('/')
+    .reply(200, '<!doctype html>')
+  })
+
+  it('should throw a proper error', async () => {
     try {
-      const res = await insistentReq('get', { url: someUrlThatReturnsHtml })
+      const res = await insistentReq('get', { url: 'https://example.org' })
       shouldNotGetHere(res)
     } catch (err) {
       rethrowShouldNotGetHereErrors(err)
-      err.message.should.startWith('Could not parse response: <!DOCTYPE html>')
+      err.message.should.equal('Could not parse response: <!doctype html>')
       err.name.should.equal('wrong response format')
     }
   })
