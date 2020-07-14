8935493ecfebd266d3c08a215bb074c1b41598c3
maxlath <k@maxlath.eu>
2020-07-14 08:54:27 +0200

    test: token_expiration: async/await refactor

--- test/integration/token_expiration.js
+++ test/integration/token_expiration.js
@@ -5,25 +5,23 @@ const wbEdit = __.require('.')(config)
 const { randomString } = __.require('test/unit/utils')
 const { getSandboxItemId } = __.require('test/integration/utils/sandbox_entities')
 const language = 'fr'
+const { wait } = __.require('test/integration/utils/utils')
 
 describe('token expiration', function () {
   this.timeout(24 * 60 * 60 * 1000)
+
   before('wait for instance', __.require('test/integration/utils/wait_for_instance'))
 
-  xit('should renew tokens', done => {
-    getSandboxItemId()
-    .then(id => {
-      const doActionRequiringAuthPeriodically = () => {
-        const value = randomString()
-        return wbEdit.alias.add({ id, language, value })
-        .then(res => {
-          res.success.should.equal(1)
-          console.log(new Date().toISOString(), 'added alias', value)
-          setTimeout(doActionRequiringAuthPeriodically, 60 * 1000)
-        })
-        .catch(done)
-      }
-      doActionRequiringAuthPeriodically()
-    })
+  xit('should renew tokens', async () => {
+    const id = await getSandboxItemId()
+    const doActionRequiringAuthPeriodically = async () => {
+      const value = randomString()
+      const res = await wbEdit.alias.add({ id, language, value })
+      res.success.should.equal(1)
+      console.log(new Date().toISOString(), 'added alias', value)
+      await wait(60 * 1000)
+      return doActionRequiringAuthPeriodically()
+    }
+    await doActionRequiringAuthPeriodically()
   })
 })
