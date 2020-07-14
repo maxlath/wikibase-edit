96a40966348b9c9c1f94a68ebc0e8d6701bfad28
maxlath <k@maxlath.eu>
2020-07-14 12:50:35 +0200

    tests: token_expiration: add different tests for the different authentification methods

--- test/integration/token_expiration.js
+++ test/integration/token_expiration.js
@@ -1,7 +1,7 @@
 require('should')
 const config = require('config')
-const { __ } = config
-const wbEdit = __.require('.')(config)
+const { __, instance, credentials, credentialsAlt } = config
+const WBEdit = __.require('.')
 const { randomString } = __.require('test/unit/utils')
 const { getSandboxItemId } = __.require('test/integration/utils/sandbox_entities')
 const language = 'fr'
@@ -12,7 +12,22 @@ describe('token expiration', function () {
 
   before('wait for instance', __.require('test/integration/utils/wait_for_instance'))
 
-  xit('should renew tokens', async () => {
+  xit('should renew tokens (oauth)', async () => {
+    const wbEdit = WBEdit({ instance, credentials })
+    const id = await getSandboxItemId()
+    const doActionRequiringAuthPeriodically = async () => {
+      const value = randomString()
+      const res = await wbEdit.alias.add({ id, language, value })
+      res.success.should.equal(1)
+      console.log(new Date().toISOString(), 'added alias', value)
+      await wait(60 * 1000)
+      return doActionRequiringAuthPeriodically()
+    }
+    await doActionRequiringAuthPeriodically()
+  })
+
+  xit('should renew tokens (username/password)', async () => {
+    const wbEdit = WBEdit({ instance, credentials: credentialsAlt })
     const id = await getSandboxItemId()
     const doActionRequiringAuthPeriodically = async () => {
       const value = randomString()
