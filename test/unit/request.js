7adec8d0c2ef7180e199940d9760f5826e60dbf7
maxlath <k@maxlath.eu>
2020-11-30 12:42:52 +0100

    requests: add a getJson function
    which is based on the main request lib (renamed from insistentReq),
    which throws nicer errors when a JSON request returns HTML
    
    addressing https://github.com/maxlath/wikibase-edit/issues/58

--- test/unit/request.js
+++ test/unit/request.js
@@ -1,10 +1,10 @@
 require('should')
 const { __ } = require('config')
-const insistentReq = __.require('lib/request/insistent_req')
+const request = __.require('lib/request/request')
 const nock = require('nock')
 const { shouldNotBeCalled, rethrowShouldNotBeCalledErrors } = require('../integration/utils/utils')
 
-describe('insistent_req', () => {
+describe('request', () => {
   beforeEach(() => {
     nock('https://example.org')
     .get('/')
@@ -13,7 +13,7 @@ describe('insistent_req', () => {
 
   it('should throw a proper error', async () => {
     try {
-      await insistentReq('get', { url: 'https://example.org' }).then(shouldNotBeCalled)
+      await request('get', { url: 'https://example.org' }).then(shouldNotBeCalled)
     } catch (err) {
       rethrowShouldNotBeCalledErrors(err)
       err.message.should.equal('Could not parse response: <!doctype html>')
