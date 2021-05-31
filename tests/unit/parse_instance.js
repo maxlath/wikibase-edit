6d469855604624ce1e5da2ec53ab7954c2059b27
maxlath <k@maxlath.eu>
2021-05-31 17:07:22 +0200

    config: take a wgScriptPath parameter
    addressing https://github.com/maxlath/wikibase-edit/issues/69

--- tests/unit/parse_instance.js
+++ tests/unit/parse_instance.js
@@ -8,12 +8,21 @@ describe('parseInstance', () => {
     parseInstance.bind(null, {}).should.throw('missing config parameter: instance')
   })
 
-  it('return an instance and sparql endpoint', () => {
+  it('should return an instance and sparql endpoint', () => {
     const configA = { instance }
     const configB = { instance: apiEndpoint }
     parseInstance(configA)
     parseInstance(configB)
     configA.instance.should.equal(instance)
     configB.instance.should.equal(instance)
+  })
+
+  it('should allow to customize the script path', () => {
+    const configA = { instance, wgScriptPath: 'foo' }
+    const configB = { instance, wgScriptPath: '/foo' }
+    parseInstance(configA)
+    configA.instanceApiEndpoint.should.equal(`${instance}/foo/api.php`)
+    parseInstance(configB)
+    configB.instanceApiEndpoint.should.equal(`${instance}/foo/api.php`)
   })
 })
