8fa782b5b506b1b8ede94e9a6f5b140334d74a14
maxlath <k@maxlath.eu>
2025-08-25 14:28:21 +0200

    utils: add types

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,34 +1,52 @@
+import type { AbsoluteUrl } from './types/common'
+
 const stringNumberPattern = /^(-|\+)?\d+(\.\d+)?$/
 const signedStringNumberPattern = /^(-|\+)\d+(\.\d+)?$/
 
-export const stringifyQuery = query => new URLSearchParams(query).toString()
+type Query = Record<string, string | number>
+
+export function stringifyQuery (query: Query) {
+  // @ts-expect-error
+  return new URLSearchParams(query).toString()
+}
 
 export type NonEmptyString = Exclude<string, ''>
 export function isNonEmptyString (str: unknown): str is NonEmptyString {
   return typeof str === 'string' && str.length > 0
 }
 
-export function buildUrl (base, query) {
+export function buildUrl (base: AbsoluteUrl, query: Query) {
   return `${base}?${stringifyQuery(query)}`
 }
 // helpers to simplify polymorphisms
-export function forceArray (obj) {
+export function forceArray <T> (obj: T | T[]): T[] {
   if (obj == null) return []
   if (!(obj instanceof Array)) return [ obj ]
   return obj
 }
-export const isString = str => typeof str === 'string'
-export const isNumber = num => typeof num === 'number'
-export const isStringNumber = str => stringNumberPattern.test(str)
-export const isSignedStringNumber = str => signedStringNumberPattern.test(str)
-export const isArray = array => array instanceof Array
+export const isString = (str: unknown) => typeof str === 'string'
+export const isNumber = (num: unknown) => typeof num === 'number'
+export function isStringNumber (str: string): str is `${number}` {
+  return stringNumberPattern.test(str)
+}
+
+type Sign = '-' | '+'
+type SignNumber = `${Sign}${number}`
+export function isSignedStringNumber (str: string): str is SignNumber {
+  return signedStringNumberPattern.test(str)
+}
+export const isArray = (array: unknown) => array instanceof Array
 export function isPlainObject (obj: unknown): obj is object {
   if (obj instanceof Array) return false
   if (obj === null) return false
   return typeof obj === 'object'
 }
-export const isntEmpty = value => value != null
-export const flatten = arrays => [].concat(...arrays)
+export function isntEmpty (value: unknown): value is Exclude<unknown, null | undefined> {
+  return value != null
+}
+export function flatten <T> (arrays: (T | T[])[]): T[] {
+  return [].concat(...arrays)
+}
 export function map (obj, fn) {
   function aggregator (index, key) {
     index[key] = fn(key, obj[key])
@@ -36,12 +54,19 @@ export function map (obj, fn) {
   }
   return Object.keys(obj).reduce(aggregator, {})
 }
-export const values = obj => Object.keys(obj).map(key => obj[key])
-export const uniq = array => Array.from(new Set(array))
-export function difference (values, excluded) {
-  return values.filter(value => !excluded.includes(value))
+
+export function values <Obj> (obj: Obj) {
+  return Object.values(obj) as Obj[keyof Obj][]
 }
-export const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+export function uniq <T> (array: T[]) {
+  return Array.from(new Set(array))
+}
+
+export function difference <A, B> (values: A[], excluded: B[]): Exclude<A, B> {
+  return values.filter(value => !arrayIncludes(excluded, value))
+}
+export const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 // Work around the TS2345 error when using Array include method
 // https://stackoverflow.com/questions/55906553/typescript-unexpected-error-when-using-includes-with-a-typed-array/70532727#70532727
