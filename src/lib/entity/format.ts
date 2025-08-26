6cb3935ca799676b234410f099400f8809c84135
maxlath <k@maxlath.eu>
2025-08-26 13:55:53 +0200

    entity format: add some types

--- src/lib/entity/format.ts
+++ src/lib/entity/format.ts
@@ -3,7 +3,12 @@ import { isString, forceArray, isntEmpty, flatten } from '../utils.js'
 import { validateAliases, validateBadges, validateLabelOrDescription, validateLanguage, validateProperty, validateSite, validateSiteTitle } from '../validate.js'
 import buildClaim from './build_claim.js'
 import reconcileClaim from './reconcile_claim.js'
-import type { SitelinkBadges } from 'wikibase-sdk'
+import type { Reconciliation } from './validate_reconciliation_object.js'
+import type { PropertiesDatatypes } from '../properties/fetch_properties_datatypes.js'
+import type { AbsoluteUrl } from '../types/common.js'
+import type { CustomSimplifiedClaim, PropertyId, SimplifiedClaim, SimplifiedClaims, SimplifiedPropertyClaims, SimplifiedSitelinks, SimplifiedTerm, Sitelink, SitelinkBadges, SitelinkTitle, Term, WikimediaLanguageCode } from 'wikibase-sdk'
+
+type CustomSimplifiedClaims = Record<PropertyId, SimplifiedPropertyClaims | SimplifiedClaim | CustomSimplifiedClaim[] | CustomSimplifiedClaim>
 
 function formatBadgesArray (badges: SitelinkBadges | string) {
   let badgeArray: SitelinkBadges
@@ -33,13 +38,13 @@ export function formatValues (name: string, values: string | string) {
   return obj
 }
 
-export function formatClaims (claims, properties, instance, reconciliation, existingClaims) {
+export function formatClaims (claims: CustomSimplifiedClaims, properties: PropertiesDatatypes, instance: AbsoluteUrl, reconciliation: Reconciliation, existingClaims: SimplifiedClaims) {
   if (!properties) throw newError('expected properties')
   return Object.keys(claims)
   .reduce(formatClaimFactory(claims, properties, instance, reconciliation, existingClaims), {})
 }
 
-export function formatSitelinks (sitelinks) {
+export function formatSitelinks (sitelinks: SimplifiedSitelinks) {
   const obj = {}
   Object.keys(sitelinks).forEach(site => {
     validateSite(site)
@@ -56,7 +61,7 @@ export function formatSitelinks (sitelinks) {
 }
 export const formatBadges = formatBadgesArray
 
-function formatClaimFactory (claims, properties, instance, reconciliation, existingClaims) {
+function formatClaimFactory (claims: CustomSimplifiedClaims, properties: PropertiesDatatypes, instance: AbsoluteUrl, reconciliation: Reconciliation, existingClaims: SimplifiedClaims) {
   return function formatClaim (obj, property) {
     if (!properties) throw newError('expected properties')
     if (!instance) throw newError('expected instance')
@@ -75,45 +80,63 @@ function formatClaimFactory (claims, properties, instance, reconciliation, exist
   }
 }
 
-function buildLanguageValue (value, language) {
+interface EditedTerm {
+  language: WikimediaLanguageCode
+  value: string
+  add?: boolean | string
+  remove?: boolean
+}
+
+function buildLanguageValue (value: SimplifiedTerm | Term, language) {
   // Re-building an object to avoid passing any undesired key/value
-  const valueObj = { language }
+  const valueObj: Partial<EditedTerm> = { language }
   if (isString(value)) {
     valueObj.value = value
   } else if (value === null) {
     valueObj.remove = true
   } else {
-    valueObj.value = value.value || value.title
-    if (value.remove === true) valueObj.remove = true
-    if (value.add != null) valueObj.add = ''
+    valueObj.value = value.value
+    if ('remove' in value && value.remove === true) valueObj.remove = true
+    if ('add' in value && value.add != null) valueObj.add = ''
   }
-  return valueObj
+  return valueObj as EditedTerm
 }
 
-function buildSiteTitle (title, site) {
+interface EditedSitelink {
+  // Not using Wikimedia Site type to accept non-Wikimedia sitelinks
+  site: string
+  title: SitelinkTitle
+  badges: SitelinkBadges
+  url?: AbsoluteUrl
+  remove?: boolean
+}
+
+function buildSiteTitle (title: SitelinkTitle | Sitelink, site: string) {
   // Re-building an object to avoid passing any undesired key/value
-  const valueObj = { site }
+  const valueObj: Partial<EditedSitelink> = { site }
   if (isString(title)) {
     valueObj.title = title
   } else {
-    valueObj.title = title.title || title.value
+    valueObj.title = title.title
     if (title.badges) {
       valueObj.badges = formatBadgesArray(title.badges)
     }
-    if (title.remove === true) valueObj.remove = true
+    if ('remove' in title && title.remove === true) valueObj.remove = true
   }
   return valueObj
 }
 
-function validateReconciledClaims (propertyClaims) {
+function validateReconciledClaims (propertyClaims: SimplifiedClaim[] | CustomSimplifiedClaim[]) {
   const claimsByGuid = {}
   for (const claim of propertyClaims) {
-    const { id } = claim
-    if (id) {
-      if (claimsByGuid[id] != null) {
-        throw newError('can not match several times the same claim', { claim })
-      } else {
-        claimsByGuid[id] = claim
+    if (typeof claim === 'object' && 'id' in claim) {
+      const { id } = claim
+      if (id) {
+        if (claimsByGuid[id] != null) {
+          throw newError('can not match several times the same claim', { claim })
+        } else {
+          claimsByGuid[id] = claim
+        }
       }
     }
   }
--- src/lib/get_entity.ts
+++ src/lib/get_entity.ts
@@ -1,8 +1,10 @@
 import { newError } from './error.js'
 import WBK from './get_instance_wikibase_sdk.js'
 import getJson from './request/get_json.js'
+import type { SerializedConfig } from './types/config.js'
+import type { EntityId, Props } from 'wikibase-sdk'
 
-const getEntity = async (id, props, config) => {
+async function getEntity (id: EntityId, props: Props, config: SerializedConfig) {
   const { instance } = config
   const url = WBK(instance).getEntities({ ids: id, props })
   const headers = { 'user-agent': config.userAgent }
@@ -14,13 +16,13 @@ const getEntity = async (id, props, config) => {
   return entity
 }
 
-export const getEntityClaims = async (id, config) => {
+export async function getEntityClaims (id: EntityId, config: SerializedConfig) {
   const entity = await getEntity(id, 'claims', config)
   const { statementsKey } = config
   return entity[statementsKey]
 }
 
-export const getEntitySitelinks = async (id, config) => {
+export async function getEntitySitelinks (id: EntityId, config: SerializedConfig) {
   const entity = await getEntity(id, 'sitelinks', config)
   return entity.sitelinks
 }
