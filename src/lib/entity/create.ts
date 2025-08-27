20a958fbd6c144157e6ce1c08d2fbedd9e671d3a
maxlath <k@maxlath.eu>
2025-08-27 13:08:26 +0200

    entity edit/create: add some types

--- src/lib/entity/create.ts
+++ src/lib/entity/create.ts
@@ -1,22 +1,11 @@
 import { newError } from '../error.js'
-import { editEntity } from './edit.js'
+import { editEntity, type EditEntityParams } from './edit.js'
 import type { PropertiesDatatypes } from '../properties/fetch_properties_datatypes.js'
 import type { AbsoluteUrl } from '../types/common.js'
 import type { SerializedConfig } from '../types/config.js'
-import type { DataType, Entity, EntityType, SimplifiedAliases, SimplifiedClaims, SimplifiedDescriptions, SimplifiedLabels, SimplifiedSitelinks } from 'wikibase-sdk'
+import type { Entity } from 'wikibase-sdk'
 
-export interface CreateEntityParams {
-  type: EntityType
-  datatype?: DataType
-  labels?: SimplifiedLabels
-  descriptions?: SimplifiedDescriptions
-  aliases?: SimplifiedAliases
-  claims?: SimplifiedClaims
-  statements?: SimplifiedClaims
-  sitelinks?: SimplifiedSitelinks
-}
-
-export async function createEntity (params: CreateEntityParams, properties: PropertiesDatatypes, instance: AbsoluteUrl, config: SerializedConfig) {
+export async function createEntity (params: EditEntityParams, properties: PropertiesDatatypes, instance: AbsoluteUrl, config: SerializedConfig) {
   if ('id' in params && params.id) {
     throw newError("a new entity can't already have an id", { id: params.id })
   }
--- src/lib/entity/edit.ts
+++ src/lib/entity/edit.ts
@@ -1,36 +1,69 @@
-import { isEntityId, type EntityId } from 'wikibase-sdk'
+import { omit } from 'lodash-es'
+import { isEntityId, type Entity, type Item, type ItemId, type Lexeme, type LexemeId, type MediaInfo, type MediaInfoId, type Property, type PropertyId, type SimplifiedClaims, type SimplifiedEntity } from 'wikibase-sdk'
 import { newError } from '../error.js'
 import { getEntityClaims } from '../get_entity.js'
-import { forceArray } from '../utils.js'
+import { arrayIncludes, forceArray } from '../utils.js'
 import { formatClaims, formatSitelinks, formatValues } from './format.js'
 import { isIdAliasPattern, resolveIdAlias } from './id_alias.js'
-import type { CreateEntityParams, CreateEntityResponse } from './create.js'
+import type { CreateEntityResponse } from './create.js'
 import type { Reconciliation } from './validate_reconciliation_object.js'
 import type { PropertiesDatatypes } from '../properties/fetch_properties_datatypes.js'
 import type { AbsoluteUrl } from '../types/common.js'
 import type { SerializedConfig } from '../types/config.js'
 
-export interface EditEntityParams extends CreateEntityParams {
-  id?: EntityId
+interface EditEntityParamsBase {
+  id?: ItemId | PropertyId | LexemeId | MediaInfoId
   clear?: boolean
   create?: boolean
-  rawMode?: boolean
   reconciliation?: Reconciliation
 }
 
-export async function editEntity (data: EditEntityParams, properties: PropertiesDatatypes, instance: AbsoluteUrl, config: SerializedConfig) {
-  validateParameters(data)
+type EditEntityRawModeParams = EditEntityParamsBase & Entity & {
+  rawMode: true
+}
 
-  let { id } = data
-  const { create, type, datatype, clear, rawMode, reconciliation } = data
-  const params = { data: {} }
-  let existingClaims
+type EditEntitySimplifiedModeParams = EditEntityParamsBase & SimplifiedEntity & {
+  rawMode: false | undefined
+}
 
-  if (type && type !== 'property' && type !== 'item') {
+export type EditEntityParams = EditEntityRawModeParams | EditEntitySimplifiedModeParams
+
+const editableTypes = [
+  // 'form',
+  'item',
+  'lexeme',
+  'mediainfo',
+  'property',
+  // 'sense',
+] as const
+
+interface WbeditentityDataBase <T extends Entity> {
+  type: T['type']
+  new?: T['type']
+  clear?: boolean
+  id?: T['id']
+  data: Partial<T>
+}
+
+type WbeditentityItemData = WbeditentityDataBase<Item>
+type WbeditentityPropertyData = WbeditentityDataBase<Property>
+type WbeditentityLexemeData = WbeditentityDataBase<Lexeme>
+type WbeditentityMediaInfoData = WbeditentityDataBase<MediaInfo>
+type WbeditentityData = WbeditentityItemData | WbeditentityPropertyData | WbeditentityLexemeData | WbeditentityMediaInfoData
+
+export async function editEntity (inputParams: EditEntityParams, properties: PropertiesDatatypes, instance: AbsoluteUrl, config: SerializedConfig) {
+  validateParameters(inputParams)
+
+  let { id } = inputParams
+  const { create, type = 'item', clear, rawMode, reconciliation } = inputParams
+  const datatype = 'datatype' in inputParams ? inputParams.datatype : undefined
+
+  if (!arrayIncludes(editableTypes, type)) {
     throw newError('invalid entity type', { type })
   }
 
-  const statements = data.claims || data.statements
+  let params: Partial<WbeditentityData> = { type, data: {} }
+  let existingClaims
 
   if (create) {
     if (type === 'property') {
@@ -38,8 +71,13 @@ export async function editEntity (data: EditEntityParams, properties: Properties
       if (!datatypes.has(datatype)) {
         throw newError('invalid property datatype', { datatype, knownDatatypes: datatypes })
       }
-      params.new = 'property'
-      params.data.datatype = datatype
+      params = {
+        ...params,
+        new: 'property',
+        data: {
+          datatype,
+        },
+      } as WbeditentityPropertyData
     } else {
       if (datatype) {
         throw newError("an item can't have a datatype", { datatype })
@@ -51,27 +89,43 @@ export async function editEntity (data: EditEntityParams, properties: Properties
       id = await resolveIdAlias(id, instance)
     }
     params.id = id
-    if (hasReconciliationSettings(reconciliation, statements)) {
+    // @ts-expect-error
+    if (hasReconciliationSettings(reconciliation, inputParams.claims || inputParams.statements)) {
       existingClaims = await getEntityClaims(id, config)
     }
   } else {
     throw newError('invalid entity id', { id })
   }
 
-  const { labels, aliases, descriptions, sitelinks } = data
+  // @ts-expect-error
+  const { labels, aliases, descriptions, claims, statements, sitelinks } = inputParams
 
   if (rawMode) {
-    if (labels) params.data.labels = labels
-    if (aliases) params.data.aliases = aliases
-    if (descriptions) params.data.descriptions = descriptions
-    if (statements) params.data.claims = statements
-    if (sitelinks) params.data.sitelinks = sitelinks
+    if (params.type === 'item' || params.type === 'property') {
+      if (labels) params.data.labels = labels
+      if (aliases) params.data.aliases = aliases
+      if (descriptions) params.data.descriptions = descriptions
+      if (claims) params.data.claims = claims
+    }
+    if (params.type === 'mediainfo') {
+      if (statements) params.data.statements = statements
+    }
+    if (params.type === 'item') {
+      if (sitelinks) params.data.sitelinks = sitelinks
+    }
   } else {
-    if (labels) params.data.labels = formatValues('label', labels)
-    if (aliases) params.data.aliases = formatValues('alias', aliases)
-    if (descriptions) params.data.descriptions = formatValues('description', descriptions)
-    if (statements) params.data.claims = formatClaims(statements, properties, instance, reconciliation, existingClaims)
-    if (sitelinks) params.data.sitelinks = formatSitelinks(sitelinks)
+    if (params.type === 'item' || params.type === 'property') {
+      if (labels) params.data.labels = formatValues('label', labels)
+      if (aliases) params.data.aliases = formatValues('alias', aliases)
+      if (descriptions) params.data.descriptions = formatValues('description', descriptions)
+      if (claims) params.data.claims = formatClaims(claims, properties, instance, reconciliation, existingClaims)
+    }
+    if (params.type === 'mediainfo') {
+      if (statements) params.data.statements = formatClaims(statements, properties, instance, reconciliation, existingClaims)
+    }
+    if (params.type === 'item') {
+      if (sitelinks) params.data.sitelinks = formatSitelinks(sitelinks)
+    }
   }
 
   if (clear === true) params.clear = true
@@ -80,12 +134,13 @@ export async function editEntity (data: EditEntityParams, properties: Properties
     throw newError('no data was passed', { id })
   }
 
-  // stringify as it will be passed as form data
-  params.data = JSON.stringify(params.data)
-
   return {
     action: 'wbeditentity',
-    data: params,
+    data: {
+      ...params,
+      // stringify as it will be passed as form data
+      data: JSON.stringify(omit(params.data, 'type')),
+    },
   }
 }
 
@@ -117,15 +172,15 @@ const allowedParameters = new Set([
   'origin',
 ])
 
-const validateParameters = data => {
-  for (const parameter in data) {
+function validateParameters (params: EditEntityParams) {
+  for (const parameter in params) {
     if (!allowedParameters.has(parameter)) {
-      throw newError(`invalid parameter: ${parameter}`, 400, { parameter, allowedParameters, data })
+      throw newError(`invalid parameter: ${parameter}`, 400, { parameter, allowedParameters, params })
     }
   }
 }
 
-const hasReconciliationSettings = (reconciliation, claims) => {
+function hasReconciliationSettings (reconciliation: Reconciliation, claims: SimplifiedClaims) {
   if (reconciliation != null) return true
   for (const property in claims) {
     for (const claim of forceArray(claims[property])) {
