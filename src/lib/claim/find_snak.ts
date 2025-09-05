49c1c657f738251cc29757648da1cb5b3327567b
maxlath <k@maxlath.eu>
2025-09-05 15:25:45 +0200

    is_matching_snak: add some types

--- src/lib/claim/find_snak.ts
+++ src/lib/claim/find_snak.ts
@@ -1,7 +1,8 @@
 import { newError } from '../error.js'
 import { isMatchingSnak } from './is_matching_snak.js'
+import type { CustomSimplifiedSnak, PropertyClaims, PropertyId } from 'wikibase-sdk'
 
-export function findSnak (property, propSnaks, searchedValue) {
+export function findSnak (property: PropertyId, propSnaks: PropertyClaims, searchedValue: CustomSimplifiedSnak['value']) {
   if (!propSnaks) return
 
   const matchingSnaks = propSnaks.filter(snak => isMatchingSnak(snak, searchedValue))
--- src/lib/claim/is_matching_snak.ts
+++ src/lib/claim/is_matching_snak.ts
@@ -1,49 +1,39 @@
-import { simplifySnak } from 'wikibase-sdk'
+import { simplifySnak, type Claim, type GlobeCoordinateSnakDataValue, type MonolingualTextSnakDataValue, type QuantitySnakDataValue, type Snak, type SnakBase, type SnakBaseWithValue, type Statement, type StringSnakDataValue, type TimeSnakDataValue, type WikibaseEntityIdSnakDataValue } from 'wikibase-sdk'
 import { newError } from '../error.js'
 import { inviteToOpenAFeatureRequest } from '../issues.js'
 import { isPlainObject, isString } from '../utils.js'
 import { parseUnit } from './quantity.js'
 
-export function isMatchingSnak (existingSnak, searchedValue) {
-  // Support both statements and qualifiers snaks
-  existingSnak = existingSnak.mainsnak ? existingSnak.mainsnak : existingSnak
-  // Support both a full snak object or just the datavalue.value object
-  searchedValue = searchedValue.datavalue ? searchedValue.datavalue.value : searchedValue
-  const { datatype } = existingSnak
-  if (searchedValue.snaktype && (searchedValue.snaktype !== 'value' || existingSnak.snaktype !== 'value')) {
-    return existingSnak.snaktype === searchedValue.snaktype
+export function isMatchingSnak (_existingSnak: Claim | Statement | Snak, _searchedValue: Snak | SnakBaseWithValue['datavalue']['value']) {
+  const existingSnak: SnakBase = 'mainsnak' in _existingSnak ? _existingSnak.mainsnak : _existingSnak
+  if (typeof _searchedValue === 'object' && 'snaktype' in _searchedValue && _searchedValue.snaktype && (_searchedValue.snaktype !== 'value' || existingSnak.snaktype !== 'value')) {
+    return existingSnak.snaktype === _searchedValue.snaktype
   }
-  if (comparatorsByDatatype[datatype] == null) {
-    const context = { datatype }
+  let searchedValue: SnakBaseWithValue['datavalue']['value']
+  if (typeof _searchedValue === 'object' && 'datavalue' in _searchedValue) {
+    searchedValue = _searchedValue.datavalue.value
+  } else {
+    searchedValue = _searchedValue as SnakBaseWithValue['datavalue']['value']
+  }
+  if (!('datavalue' in existingSnak)) return false
+  const datavalueType = existingSnak.datavalue.type
+  if (comparatorsByDatavalueType[datavalueType] == null) {
+    const context = { datavalueType }
     const featureRequestMessage = inviteToOpenAFeatureRequest({
-      title: `claim reconciliation: add support for ${datatype} datatype`,
+      title: `claim reconciliation: add support for ${datavalueType} datavalue type`,
       context,
     })
-    throw newError(`unsupported datatype: ${datatype}\n${featureRequestMessage}`, context)
+    throw newError(`unsupported datavalue type: ${datavalueType}\n${featureRequestMessage}`, context)
   }
-  return comparatorsByDatatype[datatype](existingSnak, searchedValue)
+  // @ts-expect-error
+  return comparatorsByDatavalueType[datavalueType](existingSnak, searchedValue)
 }
 
-const simpleValueComparison = (snak, searchedValue) => snak.datavalue?.value === searchedValue
-function entityValueComparison (snak, searchedValue) {
-  const { value } = snak.datavalue
-  if (typeof searchedValue === 'string') {
-    return value.id === searchedValue
-  } else if (value.id != null && searchedValue.id != null) {
-    return value.id === searchedValue.id
-  } else {
-    return value['entity-type'] === searchedValue['entity-type'] && value['numeric-id'] === searchedValue['numeric-id']
-  }
-}
-
-const comparatorsByDatatype = {
-  commonsMedia: simpleValueComparison,
-  edtf: simpleValueComparison,
-  'external-id': simpleValueComparison,
-  'geo-shape': simpleValueComparison,
-  'globe-coordinate': (snak, searchedValue) => {
-    const { latitude, longitude, altitude, precision, globe } = snak.datavalue.value
-    if (isPlainObject(searchedValue)) {
+const comparatorsByDatavalueType = {
+  globecoordinate: (snak: SnakBaseWithValue & { datavalue: GlobeCoordinateSnakDataValue }, searchedValue: GlobeCoordinateSnakDataValue['value'] | [ number, number ]) => {
+    const value = snak.datavalue.value
+    const { latitude, longitude, altitude, precision, globe } = value
+    if (typeof searchedValue === 'object' && !(searchedValue instanceof Array)) {
       if (latitude !== searchedValue.latitude) return false
       if (longitude !== searchedValue.longitude) return false
       if (precision !== searchedValue.precision) return false
@@ -56,14 +46,14 @@ const comparatorsByDatatype = {
       return latitude === searchedValue[0] && longitude === searchedValue[1]
     }
   },
-  math: simpleValueComparison,
-  'musical-notation': simpleValueComparison,
-  monolingualtext: (snak, searchedValue) => {
-    const { text, language } = snak.datavalue.value
+  monolingualtext: (snak: SnakBaseWithValue & { datavalue: MonolingualTextSnakDataValue }, searchedValue: MonolingualTextSnakDataValue['value']) => {
+    const value = snak.datavalue.value
+    const { text, language } = value
     return language === searchedValue.language && text === searchedValue.text
   },
-  quantity: (snak, searchedValue) => {
-    let { amount, lowerBound, upperBound } = snak.datavalue.value
+  quantity: (snak: (SnakBaseWithValue & { datavalue: QuantitySnakDataValue }), searchedValue: QuantitySnakDataValue['value'] | number) => {
+    const value = snak.datavalue.value
+    let { amount, lowerBound, upperBound } = value
     amount = parseAmount(amount)
     lowerBound = parseAmount(lowerBound)
     upperBound = parseAmount(upperBound)
@@ -88,11 +78,18 @@ const comparatorsByDatatype = {
       return amount === parseAmount(searchedValue)
     }
   },
-  string: simpleValueComparison,
-  'tabular-data': simpleValueComparison,
-  time: (snak, searchedValue) => {
-    const { time, timezone, before, after, precision, calendarmodel } = snak.datavalue.value
-    if (searchedValue.time != null) {
+  string: (snak: SnakBaseWithValue & { datavalue: StringSnakDataValue }, searchedValue: string) => {
+    const value = snak.datavalue.value
+    if (searchedValue.startsWith('http')) {
+      return normalizeUrl(value) === normalizeUrl(searchedValue)
+    } else {
+      return value === searchedValue
+    }
+  },
+  time: (snak: SnakBaseWithValue & { datavalue: TimeSnakDataValue }, searchedValue: TimeSnakDataValue['value'] | string) => {
+    const value = snak.datavalue.value
+    const { time, timezone, before, after, precision, calendarmodel } = value
+    if (typeof searchedValue === 'object') {
       if (searchedValue.timezone != null && searchedValue.timezone !== timezone) return false
       if (searchedValue.before != null && searchedValue.before !== before) return false
       if (searchedValue.after != null && searchedValue.after !== after) return false
@@ -106,15 +103,21 @@ const comparatorsByDatatype = {
       return simplifiedSnak === searchedValue
     }
   },
-  url: (snak, searchedValue) => normalizeUrl(snak.datavalue.value) === normalizeUrl(searchedValue),
-  'wikibase-form': entityValueComparison,
-  'wikibase-item': entityValueComparison,
-  'wikibase-lexeme': entityValueComparison,
-  'wikibase-property': entityValueComparison,
-  'wikibase-sense': entityValueComparison,
+  'wikibase-entityid': (snak: SnakBaseWithValue & { datavalue: WikibaseEntityIdSnakDataValue }, searchedValue: WikibaseEntityIdSnakDataValue['value'] | string) => {
+    const value = snak.datavalue.value
+    if (typeof searchedValue === 'string') {
+      return value.id === searchedValue
+    } else if (value.id != null && searchedValue.id != null) {
+      return value.id === searchedValue.id
+    } else {
+      return value['entity-type'] === searchedValue['entity-type'] && value['numeric-id'] === searchedValue['numeric-id']
+    }
+  },
 }
 
-const getUnit = snak => parseUnit(snak.datavalue.value.unit)
+function getUnit (snak: SnakBaseWithValue & { datavalue: QuantitySnakDataValue }) {
+  return parseUnit(snak.datavalue.value.unit)
+}
 
 const parseAmount = amount => isString(amount) ? parseFloat(amount) : amount
 
